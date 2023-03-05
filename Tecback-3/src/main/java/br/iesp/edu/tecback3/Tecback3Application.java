384cae67d8c86c142d1838a0c04d7f0f9e90df91
Icaro Raphael <icaro2551@gmail.com>
2023-03-05 13:59:49 -0300

    1.0

---
+++ Tecback-3/src/main/java/br/iesp/edu/tecback3/Tecback3Application.java
@@ -0,0 +1,13 @@
+package br.iesp.edu.tecback3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tecback3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tecback3Application.class, args);
+    }
+
+}
