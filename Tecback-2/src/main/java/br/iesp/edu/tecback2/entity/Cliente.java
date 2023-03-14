6c19f0686e076b304faa17d5a0ad0a1a21d18df8
Icaro Raphael <icaro2551@gmail.com>
2023-03-13 23:10:52 -0300

    Base do projeto do supermercado

--- Tecback-2/src/main/java/br/iesp/edu/tecback2/entity/Cliente.java
+++ Tecback-2/src/main/java/br/iesp/edu/tecback2/entity/Cliente.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Entity
 @Getter
@@ -14,6 +16,12 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message="Campo nome é obrigatório.")
     private String nome;
+
+    @CPF
+    @NotEmpty(message="Campo cpf é obrigatório.")
     private String cpf;
 }
+
